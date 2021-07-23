ee9b3cee413d1ba186e8756b5edd319542e98d26
pom421 <mauguet.po@gmail.com>
2021-07-23 17:58:37 +0200

    new API for getting info from GitHub

--- pages/api/info.js
+++ pages/api/info.js
@@ -1,14 +1,26 @@
 import config from '../../package.json';
 
 async function handler(req, res) {
-  const githubInfos = await fetch("https://api.github.com/repos/jasonrudolph/keyboard").then(r => r.json())
+  const { name, version, repository : { url } } = config;
 
-  const { name, version } = config;
+  const apiUrl = url.replace("https://github.com", "https://api.github.com/repos")
+
+  console.log("url", apiUrl)
+
+  let githubInfos
+
+  if (apiUrl) {
+   githubInfos = await fetch(apiUrl).then(r => r.json())
+  }
+
+  const {stargazers_count, forks, open_issues  } = githubInfos
 
   res.status(200).json({
     version,
     name,
-    ...githubInfos
+    stargazers_count,
+    forks,
+    open_issues
   });
 };
 
